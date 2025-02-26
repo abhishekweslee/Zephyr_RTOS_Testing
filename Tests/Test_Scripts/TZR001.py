1fd8ad9c42291a51a6de388cc98fbce412a21689
abhishekweslee <159134977+abhishekweslee@users.noreply.github.com>
2025-02-27 01:10:35 +0530

    Update TZR001.py

--- Tests/Test_Scripts/TZR001.py
+++ Tests/Test_Scripts/TZR001.py
@@ -21,7 +21,7 @@ def setup_fixture(request):
     setup.cleanup()
 
 
-def test_case(setup_fixture):
+def task_management_test(setup_fixture):
     logger.info("Starting test_case_2.")
 
     output_file_path = "Tests/Outputs/Output_files/TZR001.txt"
