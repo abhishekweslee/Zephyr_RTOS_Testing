5b39f963f3f2204a1695ce52b01e98f00ee7ed58
abhishekweslee <159134977+abhishekweslee@users.noreply.github.com>
2025-02-27 01:15:14 +0530

    Update TZR007.py

--- Tests/Test_Scripts/TZR007.py
+++ Tests/Test_Scripts/TZR007.py
@@ -19,7 +19,7 @@ def setup_fixture(request):
 
     setup.cleanup()  # Only deletes the folder
 
-def test_case(setup_fixture):
+def semaphore_synchronization_test(setup_fixture):
     logger.info("Starting semaphore test case.")
 
     output_file_path = "Tests/Outputs/Output_files/TZR007.txt"
